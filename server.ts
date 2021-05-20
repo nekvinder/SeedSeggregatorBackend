44e3ffd6762291f9f556ec2b93cdca3587eaf8e5
nekvinder <nekvinder@gmail.com>
2021-05-21 02:37:17 +0530

    file handler and jon fix

--- server.ts
+++ server.ts
@@ -1,21 +1,30 @@
 const port = 8000
-
-import * as express from 'express'
+const imagesDirName = 'Images'
+import express from 'express'
 const { PythonShell } = require('python-shell')
-var busboy = require('connect-busboy')
 var path = require('path')
 var fs = require('fs-extra')
 const { hostname } = require('os')
 const cors = require('cors')
-import { connect, createProcessRecord, getProcessRecord } from './services/db'
+var bb = require('express-busboy')
 
-export const app = require('express')()
+import { createProcessRecord, getProcessRecord } from './services/db'
 
-app.use(busboy()).use(express.json()).use(cors())
-app.use('/Images', express.static(path.join(__dirname, 'Images/')))
+export const app = express()
+
+bb.extend(app, {
+  upload: true,
+  path: __dirname + '/' + imagesDirName + '/',
+  allowedPath: /./,
+})
+
+app
+  .use(cors())
+  .use(express.json())
+  .use('/' + imagesDirName, express.static(path.join(__dirname, imagesDirName)))
 
 const getImageDirPath = (req: any) => {
-  return req.protocol + '://' + req.get('host') + '/Images/'
+  return req.protocol + '://' + req.get('host') + '/' + imagesDirName + '/'
 }
 
 app.get('/history', async (req, res, next) => {
@@ -29,41 +38,27 @@ app.post('/adminLogin', (req, res, next) => {
 
 app.post('/seedsProcess', (req, res, next) => {
   try {
-    var fstream
-    req.pipe((req as any).busboy)
-    ;(req as any).busboy.on('file', function (fieldname, file, filename) {
-      filename = filename.split('.')[0] + new Date().toISOString() + '.' + filename.split('.')[1]
-      const fileFullPath = __dirname + '/Images/' + filename
-      fstream = fs.createWriteStream(fileFullPath)
-      file.pipe(fstream)
-      fstream.on('close', function (error) {
-        console.log(error)
-      }),
-        fstream.on('close', async function () {
-          let options = {
-            mode: 'text',
-            pythonOptions: ['-u'],
-            scriptPath: '',
-            args: [fileFullPath],
-          }
-          PythonShell.run('seeds.py', options, async function (err, result) {
-            if (err) throw err
-            result = JSON.parse(result)
-            console.log(req.body)
-
-            // result.imageName = getUrl(req) + result.imageName
-            const dbData = await createProcessRecord(
-              {
-                imageName: filename,
-                title: req.body.title,
-                description: req.body.description,
-                percentages: JSON.stringify(result.percentages),
-              },
-              getImageDirPath(req)
-            )
-            res.json(dbData)
-          })
-        })
+    const file = (req as any).files.file
+    console.log(req.body, file)
+    let options = {
+      mode: 'text',
+      pythonOptions: ['-u'],
+      scriptPath: '',
+      args: [file.file],
+    }
+    PythonShell.run('seeds.py', options, async function (err, result) {
+      if (err) throw err
+      result = JSON.parse(result)
+      const dbData = await createProcessRecord(
+        {
+          imageName: [file.uuid, 'file', file.filename].join('/'),
+          title: req.body.title,
+          description: req.body.description,
+          percentages: JSON.stringify(result.percentages),
+        },
+        getImageDirPath(req)
+      )
+      res.json(dbData)
     })
   } catch (error) {
     res.json(error)
