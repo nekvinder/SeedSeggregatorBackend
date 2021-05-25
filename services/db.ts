c68be38056ab7fbb96ddb91c904e9b87c11dadda
nekvinder <nekvinder@gmail.com>
2021-05-26 00:14:09 +0530

    fix

--- services/db.ts
+++ services/db.ts
@@ -25,13 +25,13 @@ const sequelize = new Sequelize({
   password: '',
   storage: storagePath + '/db.sqlite',
   models: [ImageProcessRecord], // or [Player, Team],
+  logging: true,
 })
+
 export const connect = async () => {
   try {
     await sequelize.authenticate()
-    console.log('Connection has been established successfully.')
     await sequelize.sync()
-    console.log('All models were synchronized successfully.')
   } catch (error) {
     console.error('Unable to connect to the database:', error)
   }
